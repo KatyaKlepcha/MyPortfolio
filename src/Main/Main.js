5d9757a147001f6b98e7bc64de311a9e2a0386a0
Katya <Solianchik@yandex.ru>
2021-03-26 23:06:57 +0300

    commit: added new directories

---
+++ src/Main/Main.js
@@ -0,0 +1,12 @@
+import style from "./Main.module.css"
+
+const Main = () => {
+    return (
+        <div className={style.main} >
+            <h1>KATYA KLEPCHA</h1>
+            <h4>Front-end Developer</h4>
+            {/*<img src={'Pictures/Photo.png'}/>*/}
+        </div>
+    )
+}
+export default Main
