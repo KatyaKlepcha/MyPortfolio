5b462d472a9cba61ffb81ab3036f0e7fc556612e
Katya <Solianchik@yandex.ru>
2021-03-26 20:57:18 +0300

    added new file index.html

---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import Nav from "../Nav/Nav";
+import style from "./Header.module.css"
+
+const Header = () => {
+    return (
+        <div className={style.header}>
+            <Nav/>
+        </div>
+    )
+}
+export default Header
